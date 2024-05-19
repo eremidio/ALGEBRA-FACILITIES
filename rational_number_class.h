369cc647d6c534f14e7fd861bfe4861899a55c48
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-05-18 22:55:19 -0300

    Update rational_number_class.h

--- rational_number_class.h
+++ rational_number_class.h
@@ -1,290 +1,65 @@
-//VAMOS CRIAR UM PROGRAMA QUE IMPLEMENTA UMA CLASSE DE FRAÇÕES (NÚMEROS RACIONAIS) PARA SE TRABALHAR COM FLOATING POINT NUMBERS USANDO APENAS INTEIROS
-
-//********************************************************************************************************************************************************
-//CABEÇALHO
-#ifndef RATIONAL_NUMBER_CLASS_H
-#define RATIONAL_NUMBER_CLASS_H
-#include<iostream>
-#include<string>
-#include<type_traits>
-#include<stdint.h>
-#include<inttypes.h>
-
-//********************************************************************************************************************************************************
-//FUNÇÕES AUXILIARES
-template<typename T>
-T rational_gcd(T a, T b){
-  //Restrição a tipos inteiros
-  static_assert(std::is_integral<T>::value, "Parcelas devem ser números inteiros.\n");
-
-  //Resultado
-  if(b==0)
-    return a;
-  else
-    return rational_gcd(b, (a%b));
-                        };
+//VAMOS CRIAR UM PROGRAMA PARA TESTAR AS ROTINAS CRIADAS NO ARQUIVO rational_number_class.h
+//COMPILAR ESTE PROGRAMA COM O COMANDO: g++ -o rational_number_class rational_number_class.cpp
 
 
-template<typename T>
-T rational_lcm(T a, T b){
-
-  //Restrição a tipos inteiros
-  static_assert(std::is_integral<T>::value, "Parcelas devem ser números inteiros.\n");
-
-  //Resultado
-  return (a*b)/rational_gcd(a, b);
-
-                        };
+//Cabeçalho
+#include"rational_number_class.h"
+using namespace std;
 
 
-//********************************************************************************************************************************************************
-//CLASSE DE NÚMEROS RACIONAIS
-template<typename T>
-class rational_number{
-public:
-
-  //Membros
-  T numerator;
-  T denominator;
-
-
-  //Construtores da classe 
-  rational_number();
-  rational_number(T, T);
-  ~rational_number(){ };
-
-
-  //Métodos da classe
-  //Operações aritméticas básicas
-  rational_number<T> operator+(rational_number<T>);
-  rational_number<T> operator-(rational_number<T>);
-  rational_number<T> operator*(rational_number<T>);
-  rational_number<T> operator/(rational_number<T>);
-  rational_number<T> rationalize();
-
-  //Input/output e acesso a elementos
-  void set();
-  T get_denominator();
-  T get_numerator();
-  std::string algebraic();
-  
-  
-
-                     };
-
-//********************************************************************************************************************************************************
-//CONSTRUTORES DA CLASSE DE NÚMEROS RACIONAIS
-template<typename T>
-rational_number<T>::rational_number(){
-  //Restrição a tipos inteiros
-  static_assert(std::is_integral<T>::value, "Numerador e denominador devem ser inteiros.\n");
-                                     };
-
-
-template<typename T>
-rational_number<T>::rational_number(T a, T b){
-
-  //Restrição a tipos inteiros
-  static_assert(std::is_integral<T>::value, "Numerador e denominador devem ser inteiros.\n");
-
-  //--------------
-  numerator = a;
-  denominator = b;
-                                             };
-
-//********************************************************************************************************************************************************
-//MÉTODOS DA CLASSE DE NÚMEROS RACIONAIS
-
-//Operações aritméticas básicas
-//Racionaização de frações
-template<typename T>
-rational_number<T> rational_number<T>::rationalize(){
+//Função principal
+int main(){
 
   //Variáveis locais
-  rational_number<T> result;
-  T reducer = rational_gcd<T>(numerator, denominator);
+  rational_number<signed int> r1(2,3), r2, r3, r4, r5, r6, r7(144,480), r8(5,8), r9(12,5);
+  rational_number<int> r10, r11;
+
+  float ratio;
+  int quotient;
+  int i=15;
+  float f=0.66565;
 
   //Procedimentos
-    //Ajuste do resultado
-    result.numerator=numerator/reducer;
-    result.denominator=denominator/reducer;
-
-  //Resultado
-  return result;
-                                                    };
+    //Recebendo input do usuário
+    r2.set();
+    
+    cout<<"r1: "<<r1.algebraic()<<'\n';
+    cout<<"r2: "<<r2.algebraic()<<'\n';
 
 
+    //Racionalização de frações
+    cout<<"Forma reduzida de 144/480: "<<r7.rationalize().algebraic()<<'\n';
 
-//Adição
-template<typename T>
-rational_number<T> rational_number<T>::operator+(rational_number<T> r1){
-  
-  //Variáveis locais
-  rational_number<T> result;
-
-
-  //Procedimentos
-    //Calculando o denominador
-    result.denominator=rational_lcm<T>(denominator, r1.denominator);
-
-    //Calculando o numerador
-    result.numerator=((result.denominator/denominator)*numerator)+((result.denominator/r1.denominator)*r1.numerator);
+    //Operações aritméticas
+    r3=r1+r2; r4=r1-r2; r5=r1*r2;r6=r1/r2;
+    cout<<"r3=r1+r2: "<<r3.algebraic()<<'\n';
+    cout<<"r4=r1-r2: "<<r4.algebraic()<<'\n';
+    cout<<"r5=r1*r2: "<<r5.algebraic()<<'\n';
+    cout<<"r6=r1/r2: "<<r6.algebraic()<<'\n';
 
 
-  //Resultado
-  return result.rationalize();
-                                                                       };
+    //Conversão para números
+    cout<<"r8: "<<r8.algebraic()<<'\n';
+    cout<<"r9: "<<r9.algebraic()<<'\n';
+
+    ratio=to_number<signed int, float>(r8);
+    quotient=to_number<signed int,int>(r9);
+
+    cout<<"[float] r8: "<<ratio<<'\n';
+    cout<<"[int] r9: "<<quotient<<'\n';
+
+    //Conversão de números em frações
+    cout<<"[float] f: "<<f<<'\n';
+    cout<<"[int] f: "<<i<<'\n';
+
+    r10=to_rational<int,int>(i);
+    r11=to_rational<float,int>(f);
+
+    cout<<"r10: "<<r10.algebraic()<<'\n';
+    cout<<"r11: "<<r11.algebraic()<<'\n';
 
 
-//Subtração
-template<typename T>
-rational_number<T> rational_number<T>::operator-(rational_number<T> r1){
-  
-  //Variáveis locais
-  rational_number<T> result;
-
-
-  //Procedimentos
-    //Calculando o denominador
-    result.denominator=rational_lcm<T>(denominator, r1.denominator);
-
-    //Calculando o numerador
-    result.numerator=((result.denominator/denominator)*numerator)-((result.denominator/r1.denominator)*r1.numerator);
-
-
-  //Resultado
-  return result.rationalize();
-                                                                       };
-
-
-
-//Multiplicação
-template<typename T>
-rational_number<T> rational_number<T>::operator*(rational_number<T> r1){
-
-  //Variáveis locais
-  rational_number<T> result;
-
-
-  //Procedimentos
-    //Ajuste do resultado final
-    result.numerator=(numerator*r1.numerator);
-    result.denominator=(denominator*r1.denominator);
-
-  //Resultado
-  return result.rationalize();
-
-                                                                       };
-
-
-//Divisão
-template<typename T>
-rational_number<T> rational_number<T>::operator/(rational_number<T> r1){
-
-  //Variáveis locais
-  rational_number<T> result;
-
-
-  //Procedimentos
-    //Ajuste do resultado final
-    result.numerator=(numerator*r1.denominator);
-    result.denominator=(denominator*r1.numerator);
-
-  //Resultado
-  return result.rationalize();
-
-                                                                        };
-
-
-
-//Funções de entradas e saídas de dados
-
-//Função que define os elementos deuma fração
-template<typename T>
-void rational_number<T>::set(){
-  
-  //Recebendo input do usuário
-  std::cout<<"Numerador: ";
-  std::cin>>numerator;
-  std::cout<<"Denominador: ";
-  std::cin>>denominator;
-
-                              };
-
-
-//Funções que retornam o numerador e denominador das frações
-template<typename T>
-T rational_number<T>::get_numerator(){ return numerator; };
-
-template<typename T>
-T rational_number<T>::get_denominator(){ return denominator; };
-
-
-//Função que escreve uma fração como uma string 
-template<typename T>
-std::string rational_number<T>::algebraic(){
-
-  //Váriáveis locais
-  std::string rational_string="";
-
-  //Procedimentos
-    //Caso 1: denominador divide numerador
-    if(numerator%denominator==0)
-      rational_string+=std::to_string((numerator/denominator));
-
-    //Caso 2: denominador não divide numerador
-    else if(numerator%denominator!=0){
-      rational_string+=std::to_string(numerator);
-      rational_string+="/";
-      rational_string+=std::to_string(denominator);
-                                     };  
-
-
-  //Resultado
-  return rational_string;
-                                           };
-
-//********************************************************************************************************************************************************
-//FUNÇÕES DE CONVERSÃO DA CLASSE DE RACIONAIS PARA INTEIROS E FLOATING POINT NUMBERS
-
-//--------------------------------------------
-//DECLARAÇÕES DE FUNÇÕES
-template<typename T, typename U>
-U to_int(rational_number<T>);
-
-template<typename T, typename U>
-U to_float(rational_number<T>);
-
-template<typename T, typename U>
-U to_number(rational_number<T>);
-
-//--------------------------------------------
-//FUNÇÕES
-template<typename T, typename U>
-U to_int(rational_number<T> r){
-    return static_cast<U>(r.numerator/r.denominator);
-                              };
-
-template<typename T, typename U>
-U to_double(rational_number<T> r){
-    return static_cast<U>(r.numerator*1.0/r.denominator);
-                                 };
-
-
-
-template<typename T, typename U>
-U to_number(rational_number<T> r){
-
-  if(std::is_integral<U>::value)
-    return to_int<T, U>(r);
-  if(std::is_floating_point<U>::value)
-    return to_double<T, U>(r);
-  else
-    return 0;
-                                 };
-
-
-//********************************************************************************************************************************************************
-//FIM DO HEADER
-#endif
-
+  //Finalizando a aplicação
+  return 0;
+    
+          }
