19744cd8009a0a1b4c521fbfdf6842413bbeb72e
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2024-12-25 22:02:35 -0300

    Add files via upload
    corrected_algorithm

--- matrix_echelon_form_double.cpp
+++ matrix_echelon_form_double.cpp
@@ -1,34 +1,29 @@
-//VAMOS CRIAR UM PROGRAMA PARA TESTAR AS ROTINAS CRIADAS NO ARQUIVO matrix_echelon_form_double.h
+// VAMOS CRIAR UM PROGRAMA PARA TESTAR AS ROTINAS CRIADAS NO ARQUIVO matrix_echelon_form_double.h
 //COMPILAR ESTE PROGRAMA COM O COMANDO: g++ -o matrix_echelon_form_double matrix_echelon_form_double.cpp -O2
 
-
-//Cabeçalho
-#include"matrix_echelon_form_double.h"
+// Cabeçalho
+#include "matrix_echelon_form_double.h"
 using namespace std;
 
+// Função principal
+int main() {
+  // Variáveis locais
+  Matrix<float> matrix1, echelon_matrix;
+  int64_t row_permutations = 0;
 
-//Função principal
-int main(){
-
- //Variáveis locais
- Matrix<float> matrix1, echelon_matrix;
- int64_t row_permutations=0;
-
- //Procedimentos
-  //Recebendo input do usuário
+  // Procedimentos
+  // Recebendo input do usuário
   matrix1.set_size();
   matrix1.manual_setup();
 
-  //Calculando a matrizna sua forma escalonada
-  echelon_matrix=echelon_row_form<float>(matrix1, row_permutations);
+  // Calculando a matriz na sua forma escalonada
+  echelon_matrix = echelon_row_form<float>(matrix1, row_permutations);
 
-  cout<<"[Matriz] m:\n";
+  cout << "[Matriz] m:\n";
   matrix1.print_matrix();
-  cout<<"[Matriz escalonada] m:\n";
+  cout << "[Matriz escalonada] m:\n";
   echelon_matrix.print_matrix();
 
-
- //Finalizando a aplicação
- return 0;
-
-          }
+  // Finalizando a aplicação
+  return 0;
+}
--- matrix_echelon_form_double.h
+++ matrix_echelon_form_double.h
@@ -9,164 +9,94 @@
 //*************************************************************************************************************************************
 //DECLARAÇÃO DE FUNÇÕES
 template<typename T>
-Matrix<T> type2_row_change(Matrix<T>&, int64_t, T);
-template<typename T>
-Matrix<T> type3_row_change(Matrix<T>&, int64_t, int64_t, T);
-template<typename T>
 Matrix<T> echelon_row_form(Matrix<T>&, int64_t&);
-
 
 
 //*************************************************************************************************************************************
 //FUNÇÕES
-/*
-NOTA: Transformações do tipo 1 permuta de elementos de uma matriz, são realizados por meio da função membro .permute_rows(int1, int2) na
-classe de matrizes. Só será necessário implementar transformações do tipo 2, que consiste na multiplicação/divisão de uma linha por um
-escalar e transformações do tipo 3 que consiste na combinação linear de linhas. Essas transformações serão embutidas no algoritmo
-principal, porém implmentações independentes são providas aqui pois são subrotinas uteis em outros algoritmos de manipulação de matrizes.
-
-*/
-
-//Função que realiza uma transformação do tipo 2: reduz o elemento pivotal a 1
-template<typename T>
-Matrix<T> type2_row_change(Matrix<T>& m1, int64_t row, T pivot){
-
- //Variáveis locais
- Matrix<T> result=m1;
-
- //Procedimentos
- for(int64_t i=0; i<m1.columns; ++i){
-  if(result.matrix[row][i]>0)
-   result.matrix[row][i]=m1.matrix[row][i]/pivot;
-                                    };
-
- //Resultado
- return result;
-                                                               };
- 
-
-//Função que realiza uma transformação do tipo 3: combinação linear de linhas em uma matriz
-template<typename T>
-Matrix<T> type3_row_change(Matrix<T>& m1, int64_t row1, int64_t row2, T ratio){
-
- //Variáveis locais
- Matrix<T> result=m1;
-
- //Procedimentos
- for(int64_t i=0; i<m1.columns; ++i)
-  result.matrix[row2][i]=result.matrix[row2][i]+(result.matrix[row1][i]*ratio);
-
-
- //Resultado
- return result;                                   
-                                                                              };
-
-
-
-//Função que reduz uma matriz a sua forma escalonada reduzida usando a eliminação de Gauss-Jordan
+//Função que reduz uma matriz a sua forma escalonada
 template<typename T>
 Matrix<T> echelon_row_form(Matrix<T>& m1, int64_t& row_changes){
 
- //Variáveis locais
- Matrix<double> temp=matrix_cast<T, double>(m1);//Usaremos casting para double para evitar problemas envolvendo floating numbers
- double pivot=0.0, ratio;
- int64_t pivot_row=0, pivot_column=0, i, j;
- int64_t row_index=0, column_index=0;
+  //Variáveis locais
+  Matrix<double> float_point_matrix=matrix_cast<T, double>(m1);
+  double pivot, ratio;
+  int64_t pivot_row=0, pivot_column=0;
+  int64_t i, j, k, l;
+
+  //Procedimento
+    //Loop principal
+    while(1){
+
+      //Etapa 1: encontrando o pivot da matriz
+      pivot_search:
+      k=pivot_row;
+      pivot=float_point_matrix.matrix[pivot_row][pivot_column];
 
 
- //Procedimentos
-  //Loop principal
-  while(true){
-    pivot_search:
-    //Etapa 1: encontrando o pivot da matriz
-    for(i=row_index; i<temp.rows; ++i){
+      if(pivot==0){
 
-     if(temp.matrix[i][column_index]>0){
-      pivot_row=i;
-      pivot_column=column_index;
-      pivot=temp.matrix[pivot_row][pivot_column];
-      
-      //Condição que determina se será necessário permutar linhas da matriz a ser escalonada
-      if(pivot_row!=row_index){
-
-       row_changes++;
-      
-       //Permutando as linhas da matriz e atualizando as coordendas do pivot da matriz
-       temp.permute_rows(pivot_row, row_index);
-       pivot_row=row_index;
-      
-                              };
+        if(pivot_row==(m1.rows-1)){
+          l=pivot_column;
+          while(pivot==0){
+            l++;
+            if(l>=m1.columns) goto yield_result;
+          }
+            pivot=float_point_matrix.matrix[k][l];
+            pivot_row=k;
+            pivot_column=l;
+            goto line_reduction;
     
-      break;
-                                       }
-
-                                      };
-
-      
-
-        //TESTE USE UM /**/ APÓS O MESMO
-        /*std::cout<<"Etapa 1:\n";
-        std::cout<<"Pivot: "<<pivot<< " Pivot row: "<<pivot_row<< " Pivot column: "<<pivot_row<<'\n';
-        temp.print_matrix();*/
+        }  
 
 
-    //Etapa 2: reduzindo os elementos da coluna da matriz pelo pivot
-    line_reduction:
-    for(i=pivot_column; i<temp.columns; ++i)
-     temp.matrix[pivot_row][i]=temp.matrix[pivot_row][i]/pivot;
+        while(pivot==0){
+          k++;
+          pivot=float_point_matrix.matrix[k][pivot_column];
+          
+          //Permutando as linhas da matriz
+          if(pivot!=0) float_point_matrix.permute_rows(k, pivot_row);  
+          row_changes++;  
+        
+        } 
+      };
 
 
+      //Etapa 2: reduzindo os elementos da matriz
+      line_reduction:
+      for(j=pivot_column; j<m1.columns; ++j) float_point_matrix.matrix[pivot_row][j]=float_point_matrix.matrix[pivot_row][j]/pivot;
 
-        //TESTE USE UM /**/ APÓS O MESMO
-        /*std::cout<<"Etapa 2:\n";
-        temp.print_matrix();*/
-
-
-
-    //Condição que determina se a matriz está em sua forma escalonada
-    if(column_index==(temp.columns-1) || row_index==(temp.rows-1))
-     goto yield_result;       
-
-    //Etapa 3: checando se os elementos abaixo do pivot são todos nulos
-    column_reduction_step:
-
-    for(i=(pivot_row+1);  i<=(temp.rows-1); ++i){
-
-        ratio=temp.matrix[i][pivot_column];//Fator de redução a ser aplicado na combinação linear de linhas da matriz
+      //Condição que determina o fim do loop
+      if(pivot_row==(m1.rows-1) || pivot_column==(m1.columns-1)) break;
 
 
-        //TESTE USE UM /**/ APÓS O MESMO
-        /*std::cout<<"Ratio: "<<ratio<<'\n';*/
+      //Etapa 3: reduzindo os elementos abaixo coluna dopivot da matriz
+      for(i=(pivot_row+1); i<m1.rows; ++i){
+        if(float_point_matrix.matrix[i][pivot_column]==0) continue;
+        else{
+          ratio=float_point_matrix.matrix[i][pivot_column]*(-1);
+          for(j=0; j<m1.columns; ++j) float_point_matrix.matrix[i][j]=float_point_matrix.matrix[i][j]+(ratio*float_point_matrix.matrix[pivot_row][j]);
+
+        }
+
+      }
 
 
-
-        for(j=pivot_column; j<temp.columns; ++j)
-         temp.matrix[i][j]=temp.matrix[i][j]-(temp.matrix[pivot_row][j]*ratio);
-                                       
-                                               };
-
-
-
-        //TESTE USE UM /**/ APÓS O MESMO
-        /*td::cout<<"Etapa 3:\n";
-        temp.print_matrix();*/
-
+      //Atualizando variáveis para a próxima iteração
+      pivot_row++;
+      pivot_column++;
+    
     
 
-    //Atualizando variáveis para a próxima iteração
-    next_iteration:
-     row_index++;
-     column_index++;      
-
-             };//Fim do loop principal
+    };//Fim do loop principal
 
 
- //Resultado da execução do  algoritmo
- yield_result:
- Matrix<T> result=matrix_cast<double, T>(temp);
- return result;
-    
-                                                               };
+  //Resultado
+  yield_result:
+  Matrix<T> result=matrix_cast<double, T>(float_point_matrix);
+  return result;
+
+};
 
 
 
